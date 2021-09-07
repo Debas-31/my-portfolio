17f9d3f2123943209aee1456ca89705456d5ed88
gabrielcoder247 <gabrielcoder247@gmail.com>
2021-09-07 06:49:06 +0100

    Add function for scroll sidebar

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 /* eslint-disable linebreak-style */
 const hamburger = document.querySelector('.nav-mob');
 const sideBar = document.querySelector('.hamburger-menu');
+
 function openSideBar() {
-  sideBar.classList.add('showSideBar');
+    sideBar.classList.add('showSideBar');
 }
 
 hamburger.addEventListener('click', openSideBar);
 
 function closeSideBar() {
-  sideBar.classList.remove('showSideBar');
+    sideBar.classList.remove('showSideBar');
 }
 
 sideBar.addEventListener('click', closeSideBar);
