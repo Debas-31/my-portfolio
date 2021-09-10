2306015f97513df8b47c6df8ae5e86ddbf64b1fd
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2021-09-10 17:53:57 +0300

    Correct linters error

--- localStorage.js
+++ localStorage.js
@@ -1,16 +1,16 @@
-const emailAddress = document.getElementById("email");
-const fullName = document.getElementById("userName");
-const textMessage = document.getElementById("textMessage");
-const templateForm = document.getElementById("form");
-templateForm.addEventListener("input", () => {
+const emailAddress = document.getElementById('email');
+const fullName = document.getElementById('userName');
+const textMessage = document.getElementById('textMessage');
+const templateForm = document.getElementById('form');
+templateForm.addEventListener('input', () => {
   const localStorageObject = {
     name: fullName.value,
     email: emailAddress.value,
     message: textMessage.value,
   };
-  localStorage.setItem("templateFormData", JSON.stringify(localStorageObject));
+  localStorage.setItem('templateFormData', JSON.stringify(localStorageObject));
 });
-const getData = JSON.parse(localStorage.getItem("templateFormData"));
+const getData = JSON.parse(localStorage.getItem('templateFormData'));
 function preloaded() {
   if (getData) {
     fullName.value = getData.name;
@@ -18,6 +18,6 @@ function preloaded() {
     textMessage.value = getData.message;
     return getData;
   }
-  return "";
+  return '';
 }
 preloaded();
