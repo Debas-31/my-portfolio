554b07a7b5334f4438f75df710daec96a5e31035
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2021-09-11 22:40:43 +0300

    Modified app.js, index, style.css file and rename image file

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-//The project section
+// The project section
 
 const projectsContainer = document.querySelector('.projects-container');
 const popupProject = document.querySelector('.popup-project');
 const overlayDiv = document.querySelector('.overlay');
 
 const projects = [
-  { id: 0,
+  {
+    id: 0,
     title: 'Multi-Post stories',
     picture: 'popup-img/placeholder.png',
     modalPicture: 'popup-img/big-placeholder.png',
@@ -13,32 +14,35 @@ const projects = [
     technologies: ['html', 'Ruby', 'css'],
     liveLink: '#',
     codeLink: '#',
-  
+
   },
-  { id: 1,
+  {
+    id: 1,
     title: 'Usb flash drive',
-    picture: '.popup-img/placeholder.png',
+    picture: 'popup-img/placeholder.png',
     modalPicture: 'popup-img/big-placeholder.png',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe..",
-    technologies: ['node.js', 'bootstrap', 'sass' ],
+    technologies: ['node.js', 'bootstrap', 'sass'],
     liveLink: '#',
     codeLink: '#',
   },
-  { id: 2,
+  {
+    id: 2,
     title: 'Facebook 360',
     picture: 'popup-img/placeholder.png',
     modalPicture: 'popup-img/big-placeholder.png',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe..",
-    technologies: ['node.js', 'bootstrap', 'sass' ],
+    technologies: ['node.js', 'bootstrap', 'sass'],
     liveLink: '#',
     codeLink: '#',
   },
-  { id: 3,
+  {
+    id: 3,
     title: 'Uber Navigation',
     picture: 'popup-img/placeholder.png',
     modalPicture: 'popup-img/big-placeholder.png',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe..",
-    technologies: ['node.js', 'bootstrap', 'sass' ],
+    technologies: ['node.js', 'bootstrap', 'sass'],
     liveLink: '#',
     codeLink: '#',
   },
@@ -47,35 +51,33 @@ const projects = [
 // Display a functions
 
 function renderTechnologies(techArr) {
-  console.log(techArr);
-    let technologiesHtml = "<ul class='technologies-used'>";
-    techArr.forEach((tech, index) => {
-      technologiesHtml += `<li class='technology'>${tech}</li>`;
-      if (index !== (techArr.length - 1)) {
-        technologiesHtml += "<li class='separator'>./image/vertical-line.png alt='Line separing the technologies used in a project'></li>";
-      }
-    });
-    technologiesHtml += '</ul>';
-    return technologiesHtml;
-  }
-  console.log(technologiesHtml);
+  let technologiesHtml = "<ul class='technologies-used'>";
+  techArr.forEach((tech, index) => {
+    technologiesHtml += `<li class='technology'>${tech}</li>`;
+    if (index !== (techArr.length - 1)) {
+      technologiesHtml += "<li class='separator'><img src='image/vertical-line.png' alt='Line separing the technologies used in a project'></li>";
+    }
+  });
+  technologiesHtml += '</ul>';
+  return technologiesHtml;
+}
+// console.log(technologiesHtml);
 
-  function renderTechnologiesForModal(techArr) {
-    console.log(techArr);
-    let technologiesHtml = "<ul class='technologies-used'>";
-    techArr.forEach((tech, index) => {
-      if (index === 0) {
-        technologiesHtml += "<li class='separator'><img src='./image/vertical-line.png' alt='Line separing the technologies used in a project'></li>";
-      }
-      technologiesHtml += `<li class='technology'>${tech}</li>`;
-      technologiesHtml += "<li class='separator'><img src='./image/vertical-line.png' alt='Line separing the technologies used in a project'></li>";
-    });
-    technologiesHtml += '</ul>';
-    return technologiesHtml;
-  }
+function renderTechnologiesForModal(techArr) {
+  let technologiesHtml = "<ul class='technologies-used'>";
+  techArr.forEach((tech, index) => {
+    if (index === 0) {
+      technologiesHtml += "<li class='separator'><img src='image/vertical-line.png' alt='Line separing the technologies used in a project'></li>";
+    }
+    technologiesHtml += `<li class='technology'>${tech}</li>`;
+    technologiesHtml += "<li class='separator'><img src='image/vertical-line.png' alt='Line separing the technologies used in a project'></li>";
+  });
+  technologiesHtml += '</ul>';
+  return technologiesHtml;
+}
 
-  function renderSingleProject(p) {
-    const projectHtml = `<div class='project'>
+function renderSingleProject(p) {
+  const projectHtml = `<div class='project'>
          <div class='screenshot-container'>
            <img src='${p.picture}' alt='Screenshot of the project named ${p.title}'
               class='screenshot'>
@@ -90,16 +92,15 @@ function renderTechnologies(techArr) {
       </div>
   </div>
       `;
-    return projectHtml;
-  }
-  // Display projects dynamically in the HTML document
+  return projectHtml;
+}
+// Display projects dynamically in the HTML document
 projectsContainer.innerHTML = projects.map((p) => renderSingleProject(p)).join('');
 
 // Click event listener to projects buttons;
 const projectDetailsBtns = document.querySelectorAll('.project-details-btn');
 
 projectDetailsBtns.forEach((pdb) => {
-  console.log(pdb);
   pdb.addEventListener('click', (event) => {
     const projectId = Number(event.target.getAttribute('data-project-id'));
     const projectToShow = projects.find((p) => p.id === projectId);
@@ -116,7 +117,6 @@ projectDetailsBtns.forEach((pdb) => {
     popupProject.style.visibility = 'visible';
     popupProject.style.display = 'block';
     popupProject.style.opacity = 1;
-    disableScroll();
 
     // Popup close
     const popupClose = document.querySelector('.popup-close');
@@ -124,7 +124,6 @@ projectDetailsBtns.forEach((pdb) => {
       popupProject.style.opacity = 0;
       popupProject.style.visibility = 'hidden';
       overlayDiv.style.display = 'none';
-      enableScroll();
     });
   });
 });
