f0ea846fd53f01b31ac9ab137b9ead03a0fa34fe
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2021-09-11 14:46:14 +0300

    Modify app and index file

--- app.js
+++ app.js
@@ -7,38 +7,38 @@ const overlayDiv = document.querySelector('.overlay');
 const projects = [
   { id: 0,
     title: 'Multi-Post stories',
-    picture: 'image/Img-placeholder-project-moblie.png',
+    picture: 'image/Img-placeholder-project.png',
     modalPicture: 'image/Img-placeholder-project-moblie.png',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe..",
-    technologies: [html, Ruby, css],
+    technologies: ['html', 'Ruby', 'css'],
     liveLink: 'https://google.com',
     codeLink: 'https://github.com',
   
   },
   { id: 1,
     title: 'Usb flash drive',
-    picture: 'popup-img/flas-drive-3.png',
-    modalPicture: 'popup-img/flas-drive-3.png',
+    picture: './image/Img-placeholder-project.png',
+    modalPicture: './image/Img-placeholder-project.png',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe..",
-    technologis: [node.js, bootstrap, sass ],
+    technologis: ['node.js', 'bootstrap', 'sass' ],
     liveLink: 'https://google.com',
     codeLink: 'https://github.com',
   },
   { id: 2,
     title: 'Facebook 360',
-    picture: 'popup-img/facebook-360.png',
-    modalPicture: 'popup-img/facebook-360.png',
+    picture: 'image/Img-placeholder-project.png',
+    modalPicture: 'image/Img-placeholder-project.png',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe..",
-    technologis: [node.js, bootstrap, sass ],
+    technologis: ['node.js', 'bootstrap', 'sass' ],
     liveLink: 'https://google.com',
     codeLink: 'https://github.com',
   },
   { id: 3,
     title: 'Uber Navigation',
-    picture: 'popup-img/uber-navigation.PNG',
-    modalPicture: 'popup-img/uber-navigation.PNG',
+    picture: 'image/Img-placeholder-project.png',
+    modalPicture: 'image/Img-placeholder-project.png',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe..",
-    technologis: [node.js, bootstrap, sass ],
+    technologis: ['node.js', 'bootstrap', 'sass' ],
     liveLink: 'https://google.com',
     codeLink: 'https://github.com',
   },
@@ -46,7 +46,8 @@ const projects = [
 
 // Display a functions
 
-function displayTechnologies(techArr) {
+function renderTechnologies(techArr) {
+  console.log(techArr);
     let technologiesHtml = "<ul class='technologies-used'>";
     techArr.forEach((tech, index) => {
       technologiesHtml += `<li class='technology'>${tech}</li>`;
@@ -57,8 +58,10 @@ function displayTechnologies(techArr) {
     technologiesHtml += '</ul>';
     return technologiesHtml;
   }
+  console.log(technologiesHtml);
 
-  function displayTechnologiesForModal(techArr) {
+  function renderTechnologiesForModal(techArr) {
+    console.log(techArr);
     let technologiesHtml = "<ul class='technologies-used'>";
     techArr.forEach((tech, index) => {
       if (index === 0) {
@@ -71,7 +74,7 @@ function displayTechnologies(techArr) {
     return technologiesHtml;
   }
 
-  function displaySingleProject(p) {
+  function renderSingleProject(p) {
     const projectHtml = `<div class='project'>
          <div class='screenshot-container'>
            <img src='${p.picture}' alt='Screenshot of the project named ${p.title}'
@@ -82,7 +85,7 @@ function displayTechnologies(techArr) {
           <div class='project-description'>
               <p>${p.description}</p>
           </div>
-          ${displayTechnologies(p.technologies)}
+          ${renderTechnologies(p.technologies)}
           <button class='project-details-btn' type='button' data-project-id='${p.id}'>See Project</button>
       </div>
   </div>
@@ -90,12 +93,13 @@ function displayTechnologies(techArr) {
     return projectHtml;
   }
   // Display projects dynamically in the HTML document
-projectsContainer.innerHTML = projects.map((p) => displaySingleProject(p)).join('');
+projectsContainer.innerHTML = projects.map((p) => renderSingleProject(p)).join('');
 
 // Click event listener to projects buttons;
 const projectDetailsBtns = document.querySelectorAll('.project-details-btn');
 
 projectDetailsBtns.forEach((pdb) => {
+  console.log(pdb);
   pdb.addEventListener('click', (event) => {
     const projectId = Number(event.target.getAttribute('data-project-id'));
     const projectToShow = projects.find((p) => p.id === projectId);
